931c88a84145ebeb6181dc0eb64a7f2203ec642e
manoffocus117 <saifurrahmansaif954@gmail.com>
2026-02-21 00:14:55 +0600

    login system complete

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+let mobile_number_field = document.getElementById("mobile-number");
+let pin_number_field = document.getElementById("pin-number");
+let login_button = document.getElementById("login-button");
+let login_section = document.getElementById("login");
+
+login_button.addEventListener("click", function () {
+      // mobile and pin number verification
+      let mobile_number_value = mobile_number_field.value;
+      let pin_number_value = pin_number_field.value;
+
+      if (mobile_number_value.length === 11 && pin_number_value.length === 4) {
+            if (mobile_number_value && pin_number_value !== "") {
+                  login_section.style.display = "none";
+            }
+            alert("Welcome to Payoo");
+      } else {
+            alert("Invalid mobile number or pin");
+      }
+
+      // empty input fields
+      mobile_number_field.value = "";
+      pin_number_field.value = "";
+});
