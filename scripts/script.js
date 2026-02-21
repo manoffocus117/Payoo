95263a42dcc8b7838b6bece72681cf5804f62804
manoffocus117 <saifurrahmansaif954@gmail.com>
2026-02-21 23:57:10 +0600

    cash out section design complete

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,10 @@ let pin_number_field = document.getElementById("pin-number");
 let login_button = document.getElementById("login-button");
 let login_section = document.getElementById("login");
 
+let add_money_option = document.getElementById("add-money-option");
+let add_money_section = document.getElementById("add-money-section");
+
+// login system
 login_button.addEventListener("click", function () {
       // mobile and pin number verification
       let mobile_number_value = mobile_number_field.value;
@@ -21,3 +25,8 @@ login_button.addEventListener("click", function () {
       mobile_number_field.value = "";
       pin_number_field.value = "";
 });
+
+// add money section 
+add_money_option.addEventListener("click", function () {
+      add_money_section.style.display = "block";
+})
